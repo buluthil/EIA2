3645cf89cd2cd9746f20a9cb0aff79d78f48a546
hilalbulut <hilalbulut@hilals-mbp.eduroam.hs-furtwangen.de>
2018-10-09 16:48:34 +0200

    Vebessert

---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,12 @@
+
+        
+    function main() {
+        var i = prompt ("Gib deinen Namen ein");
+        var node : HTMLElement = document.getElementById("content");
+        node.innerHTML += "Gruezi";
+        node.innerHTML += i;
+        node.innerHTML += ",  Liebe Grueße von Hilal!";
+        console.log("Gruezi",i,", willkommen bei EIA2!");
+        }
+    
+    document.addEventListener('DOMContentLoaded',main);
