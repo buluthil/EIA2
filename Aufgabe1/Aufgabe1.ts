791d83dc74ca04d02a8df3db43a2094e4edd177a
hilalbulut <hilalbulut@hilals-mbp>
2018-10-14 20:16:08 +0200

    v

--- Aufgabe1/Aufgabe1.ts
+++ Aufgabe1/Aufgabe1.ts
@@ -1,10 +1,10 @@
-var Boxes;
-(function (Boxes) {
-    let n = 5;
-    let c;
-    let x = 0;
-    let y = 0;
-    for (let i = 0; i < n; i++) {
+namespace Boxes {
+    let n: number = 5;
+    let c: string;
+    let x: number = 0;
+    let y: number = 0;
+
+    for (let i: number = 0; i < n; i++) {
         y += (i == 2) ? 20 : 50;
         x = (x + 170) % 400;
         switch (i) {
@@ -20,16 +20,19 @@ var Boxes;
             default:
                 c = "#0000ff";
         }
-        for (let a = 50; a > 0; a -= 20) {
+        for (let a: number = 50; a > 0; a -= 20) {
             PlaceDiv(c, x, y, a, a);
             if (i == 4)
                 break;
         }
     }
-    function PlaceDiv(_color, _x, _y, _width, _height) {
-        let div = document.createElement("div");
+
+
+    function PlaceDiv(_color: string, _x: number, _y: number, _width: number, _height: number): void {
+        let div: HTMLDivElement = document.createElement("div");
         document.body.appendChild(div);
-        let s = div.style;
+
+        let s: CSSStyleDeclaration = div.style;
         s.border = "thin solid black";
         s.position = "absolute";
         s.backgroundColor = _color;
@@ -38,5 +41,4 @@ var Boxes;
         s.left = _x + "px";
         s.top = _y + "px";
     }
-})(Boxes || (Boxes = {}));
-//# sourceMappingURL=Aufgabe1.js.map
+}
